ae755bf51791ceb1de252542bdc23504bb6974e2
Josh Wright <142127801+Jw97651@users.noreply.github.com>
2024-08-21 15:10:26 +0000

    updated code

--- 01.00.09 Inputting Data.py
+++ 01.00.09 Inputting Data.py
@@ -1,5 +1,7 @@
-firstname = input()  # Input statement without a prompt
-print("You Entered: " + firstname)
+#firstname = input()  # Input statement without a prompt
+#print("You Entered: " + firstname)
+
+promt =  "Enter your Name: "
  
-lastname = input("Enter Your Name:")  # Input statement with a prompt
+lastname = input("Enter Your Name: ")  # Input statement with a prompt
 print("You Entered: " + lastname)
