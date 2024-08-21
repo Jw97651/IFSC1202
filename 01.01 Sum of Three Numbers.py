79304e2e300c75ef9504a9795d401034ed9317b4
Josh Wright <142127801+Jw97651@users.noreply.github.com>
2024-08-21 15:32:34 +0000

    update code

---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,7 @@
+FirstNumber = input("First Number: ")
+SecondNumber  = input("Second Number: ")
+ThirdNumber = input("Third number")
+
+sum = int (FirstNumber) + int(SecondNumber) + int(ThirdNumber)
+
+print(sum)2
