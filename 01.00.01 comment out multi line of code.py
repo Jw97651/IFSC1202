a2f4eeea345c65b0125ac8f2ff26ef7bf40532ed
Josh Wright <142127801+Jw97651@users.noreply.github.com>
2024-09-04 14:19:48 +0000

    updated code

---
+++ 01.00.01 comment out multi line of code.py
@@ -0,0 +1,9 @@
+# using the ''' at the begining and at the end of the code will comment out multiple lines of code.
+
+#Example
+
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break'''
---
+++ 04.00.01 Need For Loop.py
@@ -0,0 +1,19 @@
+sum = 0
+sum = sum + 1
+sum = sum + 2
+sum = sum + 3
+sum = sum + 4
+sum = sum + 5
+sum = sum + 6
+print(sum)
+
+#This loop here is adding all the numbers, use break to stop the loop 
+
+#example of break 
+
+'''fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break'''
+
