fa2018e153e368e324f57264d1725094dec05e16
Josh Wright <142127801+Jw97651@users.noreply.github.com>
2024-09-28 13:44:58 +0000

    fixed code

--- 06.Project.py
+++ 06.Project.py
@@ -1,39 +1,24 @@
-# Initialize counters
-input_count = 0
-merge_count = 0
-output_count = 0
-
-# Open output file for writing
-with open('06.Project Output File.txt', 'w') as output_file:
-    # Open input file for reading
-    with open('06.Project Input File.txt', 'r') as input_file:
-        lines = input_file.readlines()
-        
-        for line in lines:
-            input_count += 1
-            output_file.write(line)
-            
-            # Check for the merge line
-            if "**Insert Merge File Here**" in line:
-                # Open merge file for reading
-                with open('06.Project Merge File.txt', 'r') as merge_file:
-                    merge_lines = merge_file.readlines()
-                    for merge_line in merge_lines:
-                        merge_count += 1
-                        output_file.write(merge_line)
-                
-                # Break after merging
-                break
-
-        # Continue writing remaining lines of the input file
-        for line in lines[input_count:]:
-            output_file.write(line)
-
-# Count output records
-with open('06.Project Output File.txt', 'r') as output_file:
-    output_count = sum(1 for _ in output_file)
-
-# Print the counts
-print(f"{input_count} input file records")
-print(f"{merge_count} merge file records")
-print(f"{output_count} output file records")
+def merge_files(input_file_path, merge_file_path, output_file_path):
+    input_records = 0
+    merge_records = 0
+    output_records = 0
+    with open(output_file_path, 'w') as output_file:
+        with open(input_file_path, 'r') as input_file:
+            for line in input_file:
+                if line.strip() == '**Insert Merge File Here**':
+                    with open(merge_file_path, 'r') as merge_file:
+                        for merge_line in merge_file:
+                            output_file.write(merge_line)
+                            merge_records += 1
+                            output_records += 1
+                else:
+                    output_file.write(line)
+                    input_records += 1
+                    output_records += 1
+    print(f"Input Records: {input_records}")
+    print(f"Merge Records: {merge_records}")
+    print(f"Output Records: {output_records}")
+input_file_path = '06.Project Input File.txt'
+merge_file_path = '06.Project Merge File.txt'
+output_file_path = '06.Project Output File.txt'
+merge_files(input_file_path, merge_file_path, output_file_path)
